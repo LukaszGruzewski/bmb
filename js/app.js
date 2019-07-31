e292e4d5ce22995036cadb465b76ceb83789e0c7
Lukasz Gruzewski <l.gruzewski@gmail.com>
2019-07-31 11:48:15 +0200

    added slider to people section and finished styling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
+$(function () {
 
-    
+    var nextBtn = $('.next_btn');
+    var prevBtn = $('.prev_btn');
+    var allSlides = $('.slide');
+    var indexSlide = 0;
+    var lastIndex = allSlides.length - 1;
 
+    $(allSlides[indexSlide]).addClass('visible');
+
+    nextBtn.on('click', function () {
+
+        $(allSlides[indexSlide]).removeClass('visible');
+        indexSlide++;
+        if (indexSlide > lastIndex) {
+            indexSlide = 0;
+        }
+        $(allSlides[indexSlide]).addClass('visible');
+    })
+
+    prevBtn.on('click', function () {
+        $(allSlides[indexSlide]).removeClass('visible');
+        indexSlide--;
+        if (indexSlide < 0) {
+            indexSlide = lastIndex;
+        }
+        $(allSlides[indexSlide]).addClass('visible');
+    })
 
 });
-
