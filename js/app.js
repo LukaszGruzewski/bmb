aeb6ac1bb65da5740f43477037e2b5ec24a2aef9
Lukasz Gruzewski <l.gruzewski@gmail.com>
2019-08-01 16:05:29 +0200

    sliders refactoring continuation

--- js/app.js
+++ js/app.js
@@ -1,47 +1,22 @@
-$(function() {
-  var nextBtn = $('.next_btn');
-  var prevBtn = $('.prev_btn');
-  var allSlides = $('.slide');
-  var indexSlide = 0;
-  var lastIndex = allSlides.length - 1;
+document.addEventListener('DOMContentLoaded', function() {
+	var swiper = new Swiper('#swiper-people', {
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+		}
+		// pagination: {
+		// 	el: '.swiper-pagination',
+		// 	clickable: true
+		// }
+	});
 
-  $(allSlides[indexSlide]).addClass('visible');
-
-  nextBtn.on('click', function() {
-    $(allSlides[indexSlide]).removeClass('visible');
-    indexSlide++;
-    if (indexSlide > lastIndex) {
-      indexSlide = 0;
-    }
-    $(allSlides[indexSlide]).addClass('visible');
-  });
-
-  prevBtn.on('click', function() {
-    $(allSlides[indexSlide]).removeClass('visible');
-    indexSlide--;
-    if (indexSlide < 0) {
-      indexSlide = lastIndex;
-    }
-    $(allSlides[indexSlide]).addClass('visible');
-  });
-});
-
-var swiper = new Swiper('#swiper-logos', {
-  slidesPerView: 4,
-  slidesPerColumn: 2,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  }
-});
-
-var swiper = new Swiper('#swiper-people', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  }
+	var swiper = new Swiper('#swiper-logos', {
+		slidesPerView: 4,
+		slidesPerColumn: 2,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		}
+	});
 });
