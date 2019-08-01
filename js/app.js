3da72e9b8efb9126fc8cf8a0e49932e2eeeafb62
Lukasz Gruzewski <l.gruzewski@gmail.com>
2019-08-01 23:21:28 +0200

    fixed logo slider styling

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var swiper = new Swiper('#swiper-people', {
-		loop: true,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev'
-		}
-		// pagination: {
-		// 	el: '.swiper-pagination',
-		// 	clickable: true
-		// }
-	});
+  var swiper = new Swiper('#swiper-people', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
 
-	var swiper = new Swiper('#swiper-logos', {
-		slidesPerView: 4,
-		slidesPerColumn: 2,
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true
-		}
-	});
+  var swiper = new Swiper('#swiper-logos', {
+    slidesPerView: 4,
+    slidesPerColumn: 2,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
 });
